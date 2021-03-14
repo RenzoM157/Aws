cacbbc98045960b89bbf3f25b34092c2d9e26a9c
RenzoM157 <45223005+RenzoM157@users.noreply.github.com>
2021-03-14 14:29:18 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employeedb.c8cc996sfx6f.us-west-2.rds.amazonaws.com" #name of host (RDS endpoint)
+customuser = "admin" 
+custompass = "blackie619!"
+customdb = "employee" #name of database we created
+custombucket = "addemployeeee" # name of s3 bucket
+customregion = "us-west-2" # region of database
